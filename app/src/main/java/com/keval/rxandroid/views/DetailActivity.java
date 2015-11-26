b915e015fcc7d27acd020149c55e6765d4f6d10d
Keval <keval.malde@gmail.com>
2015-11-26 23:05:41 +0530

    Details in LinearLayout

--- app/src/main/java/com/keval/rxandroid/views/DetailActivity.java
+++ app/src/main/java/com/keval/rxandroid/views/DetailActivity.java
@@ -1,13 +1,9 @@
 package com.keval.rxandroid.views;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -45,7 +41,7 @@ public class DetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
+        setContentView(R.layout.activity_detail_2);
 
         ButterKnife.inject(this);
 
